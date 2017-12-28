32e4576c9fdcc941e222af731501d5b9dd34e05e
ayanc <ayanc@ayanc01.local>
2017-12-27 18:26:31 -0800

    cleaning up

--- src/com/ConverterMulti.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-/**
- * Created by ayanc on 6/3/17.
- */
-public interface ConverterMulti<A,B,C> {
-  C convert(A a, B b);
-}
--- src/com/PersonFactory.java
+++
@@ -1,8 +0,0 @@
-package com;
-
-/**
- * Created by ayanc on 6/3/17.
- */
-public interface PersonFactory<P extends Person> {
-  P create(String firstName, String lastName);
-}
