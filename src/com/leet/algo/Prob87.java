9685872cb8257f47f083c7d9c26caedf5245fc92
ayanc <ayanc@ayanc01.zillow.local>
2018-03-01 11:21:42 -0800

    wip

--- src/com/leet/algo/Prob87.java
+++ src/com/leet/algo/Prob87.java
@@ -6,6 +6,7 @@ package com.leet.algo;
 public class Prob87 {
   public boolean isScramble(String s1, String s2) {
     if (s1 == null || s2 == null || s1.length() < 0 || s2.length() < 0 || s1.length() != s2.length()) return false;
+    if (s1.equals(s2)) return true;
     int[] arr = new int[26];
 
     for (int i = 0; i < s1.length(); i++) arr[s1.charAt(i) - 'a']++;
@@ -13,6 +14,15 @@ public class Prob87 {
       if (arr[s2.charAt(i) - 'a'] == 0) return false;
       arr[s2.charAt(i) - 'a']--;
     }
-    return true;
+    for (int i = 1; i < s1.length(); i++) {
+      if (isScramble(s1.substring(0, i), s2.substring(0, i)) && isScramble(s1.substring(i), s2.substring(i))) return true;
+      if (isScramble(s1.substring(0, i), s2.substring(s2.length() - i)) && isScramble(s1.substring(i), s2.substring(0, s2.length() - i))) return true;
+    }
+    return false;
+  }
+
+  public static void main(String[] args) {
+    Prob87 prob87 = new Prob87();
+    System.out.println(prob87.isScramble("ab", "ba"));
   }
 }
