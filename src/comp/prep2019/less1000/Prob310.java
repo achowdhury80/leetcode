124035f5df56fd108e3f59e0b5e0626be7f8d42c
Ayan Chowdhury <aychowdh@microsoft.com>
2020-10-22 15:55:58 -0700

    wip

--- src/comp/prep2019/less1000/Prob310.java
+++ src/comp/prep2019/less1000/Prob310.java
@@ -2,45 +2,46 @@ package comp.prep2019.less1000;
 import java.util.*;
 public class Prob310 {
 	public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-		List<Integer> result = new ArrayList<>();
-	    if (n == 0) return result;
-	    if (n == 1){
-	      result.add(0);
-	      return result;
-	    }
-	    Map<Integer, List<Integer>> map = new HashMap<>();
-	    Map<Integer, Integer> inDegree = new HashMap<>();
-	    for (int i = 0; i < edges.length; i++) {
-	      if (!map.containsKey(edges[i][0])) map.put(edges[i][0], new ArrayList<>());
-	      inDegree.put(edges[i][0], inDegree.getOrDefault(edges[i][0], 0) + 1);
-	      map.get(edges[i][0]).add(edges[i][1]);
-	      if (!map.containsKey(edges[i][1])) map.put(edges[i][1], new ArrayList<>());
-	      inDegree.put(edges[i][1], inDegree.getOrDefault(edges[i][1], 0) + 1);
-	      map.get(edges[i][1]).add(edges[i][0]);
-	    }
-	    Queue<Integer> q = new LinkedList<>();
-	    for (Map.Entry<Integer, Integer> entry : inDegree.entrySet()) {
-	      if (entry.getValue() == 1) q.offer(entry.getKey());
-	    }
-	    while(!q.isEmpty()) {
-	    	if (map.size() < 3) {
-	    		for (int key : map.keySet()) result.add(key);
+        List<Integer> result = new ArrayList<>();
+        if (n == 1) {
+            result.add(0);
+            return result;
+        }
+        Map<Integer, List<Integer>> nexts = new HashMap<>();
+        int[] indeg = new int[n];
+        for (int[] edge : edges) {
+            indeg[edge[0]]++;
+            indeg[edge[1]]++;
+            List<Integer> l1 = nexts.getOrDefault(edge[0], new ArrayList<>());
+            l1.add(edge[1]);
+            nexts.put(edge[0], l1);
+            List<Integer> l2 = nexts.getOrDefault(edge[1], new ArrayList<>());
+            l2.add(edge[0]);
+            nexts.put(edge[1], l2);
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; i++) if (indeg[i] == 1) q.offer(i);
+        while(!q.isEmpty()) {
+            if (nexts.size() < 3) {
+                for (int key : nexts.keySet()) result.add(key);
 	    		break;
-	    	}
-	    	int size = q.size();
-	    	for (int i = 0; i < size; i++) {
-	    		int node = q.poll();
-	    		for (int next : map.get(node)) {
-	    			if (inDegree.containsKey(next)) {
-	    				int degree = inDegree.get(next) - 1;
-	    	            inDegree.put(next, degree);
-	    	            if (degree == 1) q.offer(next);
-	    			}
-	    		}
-	    		map.remove(node);
-	    		inDegree.remove(node);
-	    	}
-	    }
-	    return result;
+            }
+            int size = q.size();
+            for (int i = 0; i < size; i++) {
+	            int node = q.poll();
+	            for (int next : nexts.get(node)) {
+	                indeg[next]--;
+	                if (indeg[next] == 1) q.offer(next);
+	            }
+	            nexts.remove(node);
+            }
+        }
+        return result;
     }
+	
+	public static void main(String[] args) {
+		Prob310 prob = new Prob310();
+		System.out.println(prob.findMinHeightTrees(4, new int[][] {{1, 0}, {1, 2}, {1, 3}}));
+	}
 }
--- src/comp/prep2019/less1000/Prob576.java
+++ src/comp/prep2019/less1000/Prob576.java
@@ -1,26 +1,26 @@
 package comp.prep2019.less1000;
 public class Prob576 {
 	// O(Nmn) O(mn)
-	public int findPaths(int m, int n, int N, int i, int j) {
-		if(m < 1 || n < 1 || N <= 0) return 0;
-		int mod = (int)(1e9 + 7);
-        int[][] dirs = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; 
-        int[][] dp = new int[m][n];
-        for (int k = 0; k < N; k++) {
-        	int[][] temp = new int[m][n];
-        	for (int r = 0; r < m; r++) {
-        		for (int c = 0; c < n; c++) {
-        			for (int[] dir : dirs) {
-        	            int x = r + dir[0];
-        	            int y = c + dir[1];
-        	            if (x < 0 || x == m || y < 0 || y == n) 
-        	            	temp[r][c] = (temp[r][c] + 1) % mod;
-        	            else temp[r][c] = (temp[r][c] + dp[x][y]) % mod;
-        			}
-        		}
-        	}
-        	dp = temp;
-        }
-        return dp[i][j];
-    }
+		public int findPaths(int m, int n, int N, int i, int j) {
+			if(m < 1 || n < 1 || N <= 0) return 0;
+			int mod = (int)(1e9 + 7);
+	        int[][] dirs = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}}; 
+	        int[][] dp = new int[m][n];
+	        for (int k = 0; k < N; k++) {
+	        	int[][] temp = new int[m][n];
+	        	for (int r = 0; r < m; r++) {
+	        		for (int c = 0; c < n; c++) {
+	        			for (int[] dir : dirs) {
+	        	            int x = r + dir[0];
+	        	            int y = c + dir[1];
+	        	            if (x < 0 || x == m || y < 0 || y == n) 
+	        	            	temp[r][c] = (temp[r][c] + 1) % mod;
+	        	            else temp[r][c] = (temp[r][c] + dp[x][y]) % mod;
+	        			}
+	        		}
+	        	}
+	        	dp = temp;
+	        }
+	        return dp[i][j];
+	    }
 }
